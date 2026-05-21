2eba256afe0788b679fedf96d6b36cdb2c405c53
Hariprasath2422 <hariprasath2422@gmail.com>
2026-05-21 20:48:29 +0530

    Removed unused images

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="py-8 text-center text-sm text-muted-foreground">
       <p>
-        &copy; {new Date().getFullYear()} Pedro Machado. All rights reserved.
+        &copy; {new Date().getFullYear()} Hariprasath M. All rights reserved.
       </p>
     </footer>
   );
